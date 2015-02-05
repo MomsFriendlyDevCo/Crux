a2ed10397d2d3bc403064269f0ab56ff811b3891
Matt Carter <m@ttcarter.com>
2015-02-05 16:11:29 +1000

    Added Mongoose-scenario file to gulp-tasks

--- gulp-tasks/scenario.js
+++ gulp-tasks/scenario.js
@@ -6,9 +6,9 @@ var scenario = require('gulp-mongoose-scenario');
 * Setup the local Mongo DB with all the files located in paths.data
 */
 gulp.task('db', ['clean'], function(next) {
-	global.config = require('./config');
-	require('./config/db');
-	require('./models');
+	global.config = require('../config');
+	require('../config/db');
+	require('../models');
 
 	gulp.src('models/scenarios/setup.json')
 		.pipe(scenario({connection: db, nuke: true}))
