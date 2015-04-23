7eb9c58c6f14105163a12c5ff96adba785e5deef
Matt Carter <m@ttcarter.com>
2015-04-23 10:06:49 +1000

    Added basic server setup Changed all FIXME's to be an object reference - listing available in deploy.js

---
+++ deploy.js
@@ -0,0 +1,11 @@
+var FIXME = {
+	project: {
+		name: 'InsertProjectNameHere', // Must follow NPM naming rules (no spaces, lower case)
+		title: 'Insert Project Name Here', // Human friendly version of the above
+		repo: 'https://github.com/MomsFriendlyDevCo/Crux',
+		description: 'Insert project description here',
+	},
+	newrelic: {
+		license: 'Insert newrelic license code here',
+	},
+};
