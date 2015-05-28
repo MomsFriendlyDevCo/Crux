4ad877ba1a508573a6f2facb052beba1223bf1e3
Matt Carter <m@ttcarter.com>
2015-05-28 10:23:13 +1000

    Added Angular optimizations

--- app/app.js
+++ app/app.js
@@ -2,3 +2,16 @@ var app = angular.module('app', [
 	'ngResource',
 	'ngRoute'
 ]);
+
+
+app.config(function($compileProvider) {
+	if (!location.host.match(/^local/)) {
+		// Disabled in production for performance boost
+		$compileProvider.debugInfoEnabled(false);
+	}
+});
+
+app.config(function($httpProvider) {
+	// Enable async HTTP for performance boost
+	$httpProvider.useApplyAsync(true);
+});
