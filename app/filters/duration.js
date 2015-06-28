eda7e3dba9ba409daf1707dc92a9d9c0685c290e
Matt Carter <m@ttcarter.com>
2015-06-28 17:52:41 +1000

    Dropped moment dep + reformatted duration filter

--- app/filters/duration.js
+++ app/filters/duration.js
@@ -1,6 +1,5 @@
 /**
 * Display a ms readout as a duration
-* (requires Moment)
 * 
 * e.g. 60000 => '1m'
 * e.g. {{something | duration}}
@@ -11,6 +10,17 @@
 app.filter('duration', function() {
 	return function(value) {
 		if (!value) return null;
-		return moment.duration(value).asMinutes() + 'm'
+
+		var out = [];
+		var hours = Math.floor(value / (60 * 60 * 1000));
+		if (hours > 0) out.push(hours + 'h');
+
+		var mins = Math.floor(value % (60 * 60 * 1000) / (60 * 1000));
+		if (mins > 0) out.push(mins + 'm');
+
+		var secs = Math.floor(value % (60 * 1000) / 1000);
+		if (secs > 0) out.push(secs + 's');
+
+		return out.join(' ');
 	};
 });
