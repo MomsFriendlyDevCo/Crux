66cc9e80e18d34902c274a2926753caaf40de810
Matt Carter <m@ttcarter.com>
2015-07-23 15:42:17 +1000

    Added custom logging - should really be moved into an NPM module

--- server.js
+++ server.js
@@ -119,8 +119,32 @@ restify.defaults({
 });
 // }}}
 // Settings / Logging {{{
-var expressLog = require('express-log');
-app.use(expressLog());
+app.use(function(req, res, next){
+	var _end = res.end;
+	req.requestTime = new Date;
+	res.end = function() {
+		res.responseTime = (new Date) - req.requestTime;
+		res.end = _end;
+
+		console.log(
+			(
+				req.method == 'GET' ? colors.green('GET ') :
+				req.method == 'POST' ? colors.green.bgWhite('POST') :
+				req.method == 'PUT' ? colors.green.bgWhite('PUT '):
+				req.method == 'DELETE' ? colors.green.bgWhite('DELETE'):
+				colors.green(req.method)
+			) + ' ' +
+			(
+				res.statusCode >= 400 ? colors.red(res.statusCode) :
+				colors.grey(res.statusCode)
+			) + ' ' +
+			req.url + ' ' +
+			colors.grey(res.responseTime + 'ms')
+		);
+		res.end.apply(res, arguments);
+	};
+	next();
+});
 // }}}
 // Controllers {{{
 requireDir('./controllers');
